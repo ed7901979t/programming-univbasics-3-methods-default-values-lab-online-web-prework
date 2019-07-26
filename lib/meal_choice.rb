e2ba86c63a280400e702f773b90e383d52ef9cc3
clean-command-1851 <ed7901979t@gmail.com>
2019-07-26 20:00:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 # new program Meal Choice
 def meal_choice(veg1, veg2, protein="mean")
   puts "What a nutritious meal!" 
-  return
+   
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   
 
